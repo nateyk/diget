d2c4afbaeeab10d8135ec8dc190e8a73410b03ae
nateyk <nateyk@users.noreply.github.com>
2026-07-16 06:49:13 -0700

    test: close leaked feature output buffers

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function tearDown(): void
+    {
+        while (ob_get_level() > 1) {
+            ob_end_clean();
+        }
+
+        parent::tearDown();
+    }
 }
